1ed052636e6871d9099de8fa604d3bd2aa433209
KeeganDavis <keegan.davis96@gmail.com>
2024-02-21 15:24:21 -0600

    added more queries

--- EmployeeSQL/employee_sql_queries.sql
+++ EmployeeSQL/employee_sql_queries.sql
@@ -2,7 +2,7 @@
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
 JOIN salaries AS s
-ON (e.emp_no = s.emp_no)
+ON (e.emp_no = s.emp_no);
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT e.first_name, e.last_name, e.hire_date
@@ -14,16 +14,27 @@ WHERE EXTRACT(YEAR FROM e.hire_date) = 1986;
 SELECT dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
 FROM dept_manager AS dm
 JOIN employees AS e ON dm.emp_no = e.emp_no
-JOIN departments AS d ON dm.dept_no = d.dept_no
+JOIN departments AS d ON dm.dept_no = d.dept_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
-
+-- dept_emp, employees, departments
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+JOIN employees AS e on de.emp_no = e.emp_no
+JOIN departments AS d on de.dept_no = d.dept_no;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
-
+SELECT e.first_name, e.last_name, e.sex
+FROM employees AS e
+WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%';
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
-
+-- employees, dept_emp, departments
+SELECT d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM dept_emp AS de
+JOIN employees AS e on de.emp_no = e.emp_no
+JOIN departments AS d on de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
